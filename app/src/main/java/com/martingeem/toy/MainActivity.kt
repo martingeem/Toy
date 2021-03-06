07a79f50f8a75af8d2e256224bc180f646ba69dc
youngsong.kim <youngsong.kim@navercorp.com>
2021-03-06 15:01:02 +0900

    Initial commit

---
+++ app/src/main/java/com/martingeem/toy/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.martingeem.toy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("geem","toy first")
+    }
+}
